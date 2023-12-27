f4fbaff8568c007b9279617daf468ae7e210b49d
allenvarughese22 <allenvrghs2007@gmail.com>
2023-12-27 14:41:41 -0500

    added the home and project details page

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container, Typography } from "@mui/material";
+
+const HomePage: React.FC = () => {
+  return (
+    <Container>
+      <Typography variant="h2" gutterBottom>
+        Welcome to My Portfolio
+      </Typography>
+      <Typography variant="subtitle1">
+        Hi, I'm [oUR Name]. Here's a brief overview of my skills and interests.
+      </Typography>
+      {/* skilssssssssssssssssssssssssssssssssssssssssssss and infor */}
+    </Container>
+  );
+};
+
+export default HomePage;
