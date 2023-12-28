097e622bc1be1df47b43af9a290856436ad86ff4
allenvarughese22 <allenvrghs2007@gmail.com>
2023-12-28 16:25:48 -0500

    did changes to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import { store } from "./redux/store"; // Import your Redux store
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-)
+  document.getElementById("root")
+);
