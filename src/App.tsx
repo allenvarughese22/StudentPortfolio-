4df1db52fc262c07da64338e4a5f8adb6472929d
Noah Muhammad <noahxmuhammad@gmail.com>
2023-12-28 13:43:06 -0500

    added app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from "react";
-function App() {
-  return <div>App</div>;
-}
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
+import HomePage from './components/HomePage';
+import ProjectList from './components/ProjectList';
+import ProjectDetails from './components/ProjectDetails';
+import Contact from './components/Contact';
+import { Container } from '@mui/material';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <NavigationBar />
+      <Container style={{ marginTop: '20px' }}>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/projects" exact component={ProjectList} />
+          <Route path="/projects/:projectId" component={ProjectDetails} />
+          <Route path="/contact" component={Contact} />
+          {/* Add more routes as needed */}
+        </Switch>
+      </Container>
+    </Router>
+  );
+};
 
 export default App;
-//added some change
