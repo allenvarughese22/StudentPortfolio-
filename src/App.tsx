12d4572e27e2dc24ce124efac6561d0b07ed5b37
Noah Muhammad <noahxmuhammad@gmail.com>
2023-12-28 14:12:28 -0500

    allen found bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import NavigationBar from './components/NavigationBar';
-import HomePage from './components/HomePage';
-import ProjectList from './components/ProjectList';
-import ProjectDetails from './components/ProjectDetails';
-import Contact from './components/Contact';
-import { Container } from '@mui/material';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavigationBar from "./components/NavigationBar";
+import HomePage from "./components/HomePage";
+import ProjectList from "./components/ProjectList";
+import ProjectDetails from "./components/ProjectDetails";
+import Contact from "./components/Contact";
+import { Container } from "@mui/material";
+ 
 const App: React.FC = () => {
   return (
     <Router>
       <NavigationBar />
-      <Container style={{ marginTop: '20px' }}>
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/projects" exact component={ProjectList} />
-          <Route path="/projects/:projectId" component={ProjectDetails} />
-          <Route path="/contact" component={Contact} />
+      <Container style={{ marginTop: "20px" }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+
+          <Route path="/projects" element={<ProjectList />} />
+          <Route path="/projects/:projectId" element={<ProjectDetails />} />
+          <Route path="/contact" element={<Contact />} />
           {/* Add more routes as needed */}
-        </Switch>
+        </Routes>
       </Container>
     </Router>
   );
 };
-
+ 
 export default App;
+ 
