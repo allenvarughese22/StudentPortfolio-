69bd2be0943c2bf049a91b828577a23e9af2bacd
Noah Muhammad <noahxmuhammad@gmail.com>
2023-12-28 14:26:11 -0500

    added to project list

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -11,15 +11,31 @@ const ProjectList: React.FC = () => {
       {projects.map(project => (
         <Grid item xs={12} sm={6} md={4} key={project.id}>
           <Card>
-            <CardContent>
-              <Typography variant="h5" gutterBottom>
-                {project.title}
-              </Typography>
-              <Typography variant="body2">
-                {project.description}
-              </Typography>
-              {/* Add link details */}
-            </CardContent>
+          <CardContent>
+  <Typography variant="h5" gutterBottom>
+    {project.title}
+  </Typography>
+  <Typography variant="body2">
+    {project.description}
+  </Typography>
+  <div>
+    <strong>Links:</strong>
+    <ul>
+      <li>
+        <a href={project.liveDemoLink} target="_blank" rel="noopener noreferrer">
+          Live Demo
+        </a>
+      </li>
+      <li>
+        <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
+          GitHub Repository
+        </a>
+      </li>
+    </ul>
+  </div>
+  {/* Add more project details as needed */}
+</CardContent>
+
           </Card>
         </Grid>
       ))}
